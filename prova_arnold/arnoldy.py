6782295e777371b74a2bc47f6ce340ae29c2af0f
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2022-12-10 18:46:52 +0100

    Grafici Autovalori.
    Implementati alcuni grafici sugli auto-valori trovati e convergenza (credo).
    Test dell'esecuzione temporale dell'algoritmo in base alla precisione (da completare).
    Implementazione algoritmo di lanczos.

--- prova_arnold/arnoldy.py
+++ prova_arnold/arnoldy.py
@@ -1,6 +1,9 @@
 import numpy as np 
 from numpy import linalg as LA
-
+from scipy.linalg import norm
+import matplotlib.pyplot as plt
+import time
+from numpy import *
 
 def arnoldi_iteration(A, b, n: int, eps = 1e-12):
     
@@ -9,7 +12,7 @@ def arnoldi_iteration(A, b, n: int, eps = 1e-12):
     spanned by {b, Ab, ..., A^n b}.
 
     Arguments
-      A: m × m array
+      A: m x m array
       b: initial vector (length m)
       n: dimension of Krylov subspace, must be >= 1
     
@@ -23,23 +26,18 @@ def arnoldi_iteration(A, b, n: int, eps = 1e-12):
     print()
     print(A)
     
-    
-    
     h = np.zeros((n+1,n))
     Q = np.zeros((A.shape[0],n+1))
-     # Normalize the input vector
-    q = b/np.linalg.norm(b,2)
-    print(q)
+    q = b/np.linalg.norm(b,2)  # Normalize the input vector
     Q[:,0] =  q.transpose()  # Use it as the first Krylov vector
     print(Q)
     for k in range(1,n+1):
         print("------------------------------------------------")
         print("k",k)
         v = np.dot(A,Q[:,k-1])  # Generate a new candidate vector
-        #print("v", v)
         for j in range(k):  # Subtract the projections on previous vectors
             h[j,k-1] = np.dot(Q[:,j].T, v)
-            print(" h[j,k-1]: ", h[j,k-1])
+            print("h[j,k-1]: ", h[j,k-1])
             v = v - h[j,k-1] * Q[:,j]
         h[k,k-1] = np.linalg.norm(v,2)
         if h[k,k-1] > eps:  # Add the produced vector to the list, unless
@@ -47,6 +45,35 @@ def arnoldi_iteration(A, b, n: int, eps = 1e-12):
         else:  # If that happens, stop iterating.
             return Q, h
     return Q, h
+
+
+def lanczos(A, b, n:int):
+    """
+    Lanczos algorithm (Krylov approximation of a matrix)
+        input: 
+            A: matrix to approximate
+            v0: initial vector (should be in matrix form) 
+            k: number of Krylov steps 
+        output: 
+            V: matrix (large, N*k) containing the orthogonal vectors
+            H: matrix (small, k*k) containing the Krylov approximation of A
+    """
+    print('LANCZOS METHOD !')
+    V = np.mat(b.copy() / norm(b) )
+    alpha =  np.zeros(n)  
+    beta =  np.zeros(n+1)  
+    for m in range(n):
+        vt = np.dot(A , V[ :, m])
+        if m > 0: 
+            vt -= beta[m] * V[:, m-1]
+        alpha[m] = (V[:, m].H * vt)[0, 0]
+        vt -= alpha[m] * V[:, m]
+        beta[m+1] = norm(vt)
+        V = np.hstack((V, vt.copy() / beta[m+1]))
+    rbeta = beta[1:-1]    
+    H = np.diag(alpha) + np.diag(rbeta, 1) + np.diag(rbeta, -1)
+    return V, H
+
 
 
 def main():
@@ -80,25 +107,75 @@ def main():
                 ]
             ) 
     
-    Q, H = arnoldi_iteration(A, b, 9, eps = 1e-12)
+    Q, h = arnoldi_iteration(A, b, 10, eps = 1e-12)
     
     print()
+    
     print("This is Q")
+    # Matrix Q represent ortonormal bases of krylov spaces 
     print(Q)
     print()
+    
     print("This is H")
-    print(H)
     print()
+    print(h)
     
     u,v = LA.eigh(A)
     
-    print("This is u")
-    print(u)
-    print()
-    print("This is v")
-    print(v)
-
     
+    print("This is u")
+    # The eigenvalues in ascending order, each repeated according to its multiplicity.
+    # eigh = u.tolist()
+    # eigh.reverse()
+    # l_eigh = eigh
+    # eight_find = list(h[0])
+
+    # print(eight_find)
+    # print(l_eigh)
+    
+    # print()
+    
+    
+    # plt.plot(l_eigh)
+    # plt.plot(eight_find)
+    # plt.show()
+    
+    # plt.plot(h)
+    # plt.show()
+    
+    k = 10
+    Preps = 1000
+    Nmin = 10
+    lNmin = log(Nmin)/log(10)
+    Nmax = 2000
+    lNmax = log(Nmax)/log(10)
+    Nvals = array(logspace(lNmin, lNmax), dtype=int)
+
+    type_to_name = {float32:'float32', float64:'float64', complex64:'complex64', complex128:'complex128'}
+ 
+    for T in [float32, float64, complex64, complex128]:
+        print("current type:", type_to_name[T])
+        Tc = []
+        Tp = []
+        for n in Nvals:
+            ctime = inf
+            ptime = inf
+            for r in range(Preps):# time python 
+                t = time.time()
+                Vloc, Hloc = arnoldi_iteration(A, b ,10)
+                ptime = min(ptime, time.time()-t)
+            Tc.append(ctime)
+            Tp.append(ptime)
+
+        plt.loglog(Nvals, Tp, '-o', label="Python")
+        plt.legend(loc='best')
+        plt.xlabel('N', fontsize=16)
+        plt.ylabel('Time', fontsize=16)
+        plt.title('Timing of Arnoldi Algorithm with $k=%d$. Type: %s'%(k, type_to_name[T]))
+        ax = plt.axis()
+        plt.axis((Nmin, Nmax, ax[2], ax[3]))
+        plt.draw()
+        plt.show()
     
 if __name__ == "__main__":
     main()
